5b73b93026c27c8b9a43736db8c6440000819253
olliwh <98967791+olliwh@users.noreply.github.com>
2026-02-17 18:39:09 +0100

    auth service init

---
+++ services/service-auth/service-auth/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace service_auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from AuthController!");
+        }
+    }
+}
