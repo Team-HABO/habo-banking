aef955ef28a2f877310e564712f63bb08ff387b1
olliwh <98967791+olliwh@users.noreply.github.com>
2026-02-18 10:43:15 +0100

    fixed spelings and stuff

--- services/service-auth/service-auth/Program.cs
+++ services/service-auth/service-auth/Program.cs
@@ -12,9 +12,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
