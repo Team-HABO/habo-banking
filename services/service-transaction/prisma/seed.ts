8fadb75aea2f20d24c8cab042f56fb94bf9d24ac
Ali Mohammad <alihmohammad@hotmail.com>
2026-02-25 13:10:33 +0100

    feat: updated seed data

--- services/service-transaction/prisma/seed.ts
+++ services/service-transaction/prisma/seed.ts
@@ -22,24 +22,10 @@ async function main() {
 		}
 	});
 	await prisma.transactionType.upsert({
-		where: { name: "P2P_SEND" },
+		where: { name: "TRANSFER" },
 		update: {},
 		create: {
-			name: "P2P_SEND"
-		}
-	});
-	await prisma.transactionType.upsert({
-		where: { name: "P2P_RECEIVE" },
-		update: {},
-		create: {
-			name: "P2P_RECEIVE"
-		}
-	});
-	await prisma.transactionType.upsert({
-		where: { name: "PAYMENT" },
-		update: {},
-		create: {
-			name: "PAYMENT"
+			name: "TRANSFER"
 		}
 	});
 }
