4c2d1e5c512835ede9f0325077d3d96611f74948
olliwh <98967791+olliwh@users.noreply.github.com>
2026-05-07 10:46:04 +0200

    added jwt, updated tests and pipeline

--- services/service-view/src/index.ts
+++ services/service-view/src/index.ts
@@ -4,10 +4,21 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import mongoose from 'mongoose';
 import { typeDefs } from './graphql/typeDefs.js';
 import { resolvers } from './graphql/resolvers.js';    
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import type { Context } from './types/context.js';
 
+interface JwtPayload {
+  nameid: string; 
+  email: string;
+  aud: string[];
+  iss: string;
+}
 
 async function startServer() {
   const mongoConnectionString = process.env.MONGODB_CONNECTION_STRING;
+  const jwtSecret = process.env.JWT_SECRET;
+
   const serverHost = process.env.SERVER_HOST ?? '0.0.0.0';
   const serverPort = Number(process.env.SERVER_PORT ?? '4000');
 
@@ -19,12 +30,39 @@ async function startServer() {
     throw new Error('SERVER_PORT must be a positive number');
   }
 
+    if (!jwtSecret) {
+    throw new Error('JWT_SECRET is not set');
+  }
+
   await mongoose.connect(mongoConnectionString);
   console.log("Connected to MongoDB");
 
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer<Context>({ typeDefs, resolvers });
   const { url } = await startStandaloneServer(server, {
     listen: { host: serverHost, port: serverPort },
+    context: async ({ req }) => {
+      console.log(req.headers);
+      try {
+        const cookies = cookie.parse(req.headers.cookie || '');
+        console.log('Parsed Cookies:', cookies);
+        const token = cookies.token;
+        console.log('Extracted Token:', token);
+        if (!token) {
+          return { userId: null };
+        }
+
+        const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
+
+        return {
+          userId: decoded.nameid,
+        };
+      } catch (error: any) {
+        console.error('JWT Verification Failed:', error.message);
+        return {
+          userId: null,
+        };
+      }
+    },
   });
 
   console.log(`🚀 Server ready at ${url}`);
